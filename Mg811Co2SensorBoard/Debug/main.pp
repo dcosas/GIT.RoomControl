03d013deaea94fec271949acac832075a299c716
Deian Cosas <deian.cosas@outlook.com>
2015-10-28 12:20:44 +0200

    First commit of the Mg811-Co2 sensor interface board. -MSP430g2553 MCU reads adc output from the sensor and sends it on UART1

---
+++ Mg811Co2SensorBoard/Debug/main.pp
@@ -0,0 +1,15 @@
+# FIXED
+
+main.obj: C:/ti/msp430/MSPWare_2_21_00_39/examples/boards/MSP-EXP430G2/MSP-EXP430G2\ Software\ Examples/Source/OutOfBox_MSP-EXP430G2/MSP-EXP430G2-LaunchPad/main.c
+main.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h
+main.obj: C:/ti/ccsv6/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.6/include/intrinsics.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.6/include/intrinsics_legacy_undefs.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.6/include/stdint.h
+
+C:/ti/msp430/MSPWare_2_21_00_39/examples/boards/MSP-EXP430G2/MSP-EXP430G2\ Software\ Examples/Source/OutOfBox_MSP-EXP430G2/MSP-EXP430G2-LaunchPad/main.c: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.6/include/intrinsics.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.6/include/intrinsics_legacy_undefs.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.6/include/stdint.h: 
---
+++ Mg811Co2SensorBoard/Debug/subdir_vars.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430g2553.cmd 
+
+C_SRCS += \
+C:/ti/msp430/MSPWare_2_21_00_39/examples/boards/MSP-EXP430G2/MSP-EXP430G2\ Software\ Examples/Source/OutOfBox_MSP-EXP430G2/MSP-EXP430G2-LaunchPad/main.c 
+
+OBJS += \
+./main.obj 
+
+C_DEPS += \
+./main.pp 
+
+C_DEPS__QUOTED += \
+"main.pp" 
+
+OBJS__QUOTED += \
+"main.obj" 
+
+C_SRCS__QUOTED += \
+"C:/ti/msp430/MSPWare_2_21_00_39/examples/boards/MSP-EXP430G2/MSP-EXP430G2 Software Examples/Source/OutOfBox_MSP-EXP430G2/MSP-EXP430G2-LaunchPad/main.c" 
+
+

afbde69a36ef6d8bc45cebfb7c6921a3be0349fb
Deian Cosas <deian.cosas@outlook.com>
2015-10-28 12:23:41 +0200

    Renamed main directory

--- timers/Debug/timers.asm
+++ timers/Debug/timers.asm
@@ -1,6 +1,6 @@
 ;******************************************************************************
 ;* TI ARM C/C++ Codegen                                             PC v5.2.5 *
-;* Date/Time created: Tue Oct 27 17:18:44 2015                                *
+;* Date/Time created: Wed Oct 28 12:22:43 2015                                *
 ;******************************************************************************
 	.compiler_opts --abi=eabi --arm_vmrs_si_workaround=off --code_state=16 --diag_wrap=off --disable_dual_state --embedded_constants=on --endian=little --float_support=FPv4SPD16 --hll_source=on --object_format=elf --silicon_version=7M4 --symdebug:dwarf --symdebug:dwarf_version=3 --unaligned_access=on 
 	.thumb
@@ -300,7 +300,7 @@ lastUpdateCounter$1:
 sysTickTemp$3:
 	.bits	0,32			; sysTickTemp$3 @ 0
 
-;	C:\ti\ccsv6\tools\compiler\ti-cgt-arm_5.2.5\bin\armacpia.exe -@C:\\Users\\DCOSAS~1.SAG\\AppData\\Local\\Temp\\1175612 
+;	C:\ti\ccsv6\tools\compiler\ti-cgt-arm_5.2.5\bin\armacpia.exe -@C:\\Users\\DCOSAS~1.SAG\\AppData\\Local\\Temp\\0764412 
 	.sect	".text:WatchdogIntHandler"
 	.clink
 	.thumbfunc WatchdogIntHandler
@@ -1911,9 +1911,9 @@ $C$DW$125	.dwtag  DW_TAG_TI_branch
 	.align	4
 ||$C$SL1||:	.string	"1.0 10.20.15",0
 	.align	4
-||$C$SL2||:	.string	"Oct 27 2015",0
+||$C$SL2||:	.string	"Oct 28 2015",0
 	.align	4
-||$C$SL3||:	.string	"17:18:44",0
+||$C$SL3||:	.string	"12:22:43",0
 ;******************************************************************************
 ;* CONSTANT TABLE                                                             *
 ;******************************************************************************
